43f3b30edb5cd2f8bbbeeb58a31cfb0b60ba2f04
Yasseen Semlali <yasseen2000@gmail.com>
2019-10-23 12:54:01 -0400

    timeline

---
+++ src/main/java/com/dawsoncollege/twitterclient/business/Timeline.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dawsoncollege.twitterclient.business;
+
+import twitter4j.TwitterException;
+
+/**
+ *
+ * @author 1742811
+ */
+public interface Timeline{
+    public void updateTimeline() throws TwitterException;
+}
